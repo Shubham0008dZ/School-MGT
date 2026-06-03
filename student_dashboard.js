f87cbb7d7c24317aedca3b7f3a8a776f0f732a2b
Shubham Das <shubham347779@gmail.com>
2026-06-03 09:21:42 +0530

    Update student_dashboard.js

--- student_dashboard.js
+++ student_dashboard.js
@@ -1,4 +1,7 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyDv3nOs6E9OQOSXBywbYHJPpl_V8frIegpSmTCZFRlsh1xis6iS-SMZxEWxIqJ6s-aEw/exec';
+// DYNAMIC SCRIPT URL FROM MULTI-TENANT LOGIN
+const scriptURL = localStorage.getItem('erp_school_url');
+if(!scriptURL) { window.location.href = 'login.html'; }
+
 
 let studentData = null;
 let globalDbData = null;
